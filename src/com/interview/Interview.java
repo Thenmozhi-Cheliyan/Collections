de6b36160780d161898d3d6d3771979b721e137b
Thenmozhi-Cheliyan <98871555+Thenmozhi-Cheliyan@users.noreply.github.com>
2022-03-21 14:43:11 +0530

    Queue Commit

---
+++ src/com/interview/Interview.java
@@ -0,0 +1,13 @@
+package com.interview;
+
+public class Interview
+{
+    public static void main(String args[])
+    {
+        Candidates attendees = new Candidates();
+        attendees.getCandidates();
+        attendees.options();
+        attendees.callFor();
+
+    }
+}
